5e5d17a4375260c59dd4ea87b0ba6ddf95154fd9
aaron <schoeffa@gmail.com>
2018-10-16 22:38:04 -0400

    Added some functionality and comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 "use strict";
 {
+    // Wins declared globally so it can be altered by functions
     let wins = 0;
 
+    // Prompt to give user choice to play or not
     const startGame = () => {
         if (prompt("Do you want to play?").toLowerCase() === 'yes') {
             startCombat(prompt("Enter your name."));
@@ -11,26 +13,37 @@
         }
     };
 
+    // Function to simulate combat
     const startCombat = (username) => {
 
+        // Initializes user and Grant characters, loop condition, and potion inventory
         let userChar = genCharSheet(username, 40);
         let grantChar = genCharSheet("Grant the Mighty Chicken", 10);
         let battle = true;
+        let potions = 3;
 
+        // Displays initial stats for each character
         printChar(userChar);
         printChar(grantChar);
 
-
+        // Loop to run battle
         while (battle) {
+
+            // Initializes flags used to determine if character 2 gets his turn and whether we are at the end of battle or end of game
             let nextTurn = true;
             let status;
-            switch (prompt("Do you want to attack, cast, or flee").toLowerCase()) {
+
+            // Switch statement allowing user choice of action
+            switch (prompt("Do you want to attack, cast, heal, check stats, or flee?").toLowerCase()) {
+
+                // Allows user to flee battle
                 case "flee":
                     battle = false;
                     nextTurn = false;
                     console.log(`${userChar[0]} is intimidated by ${grantChar[0]}'s awesomeness and flees the battle.`);
                     break;
 
+                // Allows user to attack using Strength stat
                 case "attack":
                     attack("Fists of Rage", userChar, grantChar, 2);
                     status = checkVictory(userChar, grantChar, nextTurn);
@@ -41,7 +54,8 @@
                         nextTurn = false;
                     }
                     break;
-
+                
+                // Allows user to attack using Magic stat
                 case "cast":
                     attack("Growl", userChar, grantChar, 3);
                     status = checkVictory(userChar, grantChar);
@@ -52,14 +66,37 @@
                         nextTurn = false;
                     }
                     break;
+                
+                // Allows user to consume a potion, if available, to heal HP
+                case "heal":
+                    if (potions > 0) {
+                        potions--;
+                        heal(userChar);
+                    }
+                    else {
+                        console.log(`${userChar[0]} is out of potions.`)
+                        nextTurn = false;
+                    }
+                    break;
+
+                // Allows user to check current stats
+                case "check stats":
+                    printChar(userChar);
+                    nextTurn = false;
+                    break;
+                
+                // Displays if user types an invalid command 
                 default:
                     console.log("Action not allowed");
+                    nextTurn = false;
             }
 
+            // Conditional and switch statement to simulate Grant's turn
+            // Math in switch statement randomizes whether Grant uses physical attack or magic
             if (nextTurn) {
                 switch (Math.floor((Math.random() * 2) + 1)) {
                     case 1:
-                        attack("Mega Chirp", grantChar, userChar, 2);
+                        attack("Mega Peck", grantChar, userChar, 2);
                         status = checkVictory(userChar, grantChar);
                         break;
                     case 2:
@@ -73,31 +110,38 @@
                 }
             }
 
+            // Conditional to see if Grant has defeated user
             if (status === "endGame") {
                 battle = false;
             }
         }
     }
 
-
+    // Function used to assign randomized damage for attacks
     const getDamage = () => { return Math.floor((Math.random() * 5) + 1) };
 
-    const genCharSheet = (charName, charHP) => { return [charName, charHP, Math.floor((Math.random() * 2) + 1), Math.floor((Math.random() * 2) + 1)] };
+    // Function used to generate a character and corresponding stats
+    const genCharSheet = (charName, charHP) => { return [charName, charHP, Math.floor((Math.random() * 2) + 1), Math.floor((Math.random() * 2) + 1), Math.floor((Math.random() * 2) + 1)] };
 
+    // Function used to print character stats
     const printChar = (user) => {
         console.log(`${user[0]}
 HP: ${user[1]}
 Strength: ${user[2]}
-Magic: ${user[3]}`)
+Magic: ${user[3]}
+Med: ${user[4]}`)
     };
 
+    // Function used to execute attack and update character HP
     const attack = (atkName, charActive, charDefend, type) => {
         let charAttack = (getDamage() * charActive[type]);
         charDefend[1] -= charAttack;
         console.log(`${charActive[0]} uses ${atkName} and deals ${charAttack} damage.`);
+        if (charDefend[1] < 0) { charDefend[1] = 0};
         console.log(`${charDefend[0]} has ${charDefend[1]} health left.`);
     };
 
+    // Function ussed to check for battle and game victory conditions
     const checkVictory = (userChar, grantChar) => {
         if (userChar[1] <= 0) {
             console.log(`${grantChar[0]} has defeated ${userChar[0]}.`);
@@ -115,6 +159,18 @@ Magic: ${user[3]}`)
             }
         } else {
             return;
+        }
+    };
+
+    // Function used to consume potion and heal character
+    const heal = (userChar) => {
+        let potionVal = (Math.floor((Math.random() * 10) + 1) * userChar[4]);
+        userChar[1] += potionVal;
+        if (userChar[1] > 40) {
+            console.log(`${userChar[0]} drinks a potion and heals ${userChar[1] - 40}HP`);
+            userChar[1] = 40;
+        } else {
+            console.log(`${userChar[0]} drinks a potion and heals ${potionVal}HP`);
         }
     };
 
